3844bcd981c46d41d15891b687264ba889d66fd3
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-02-27 18:41:08 +0330

    hid newsletter because of mail server errors.

--- Rasta_Web/utils.py
+++ Rasta_Web/utils.py
@@ -36,6 +36,7 @@ def validate_image_size(image):
     if file_size > limit_mb * 1024 * 1024:
         raise ValidationError("Max size of file is %s MB" % limit_mb)
 
+
 def validate_square_image(image):
     width = image.width
     height = image.height
